00f6985884b594c8b380694ae866769936315b13
Matt Thomas <matt@betweenbrain.com>
2012-08-21 14:30:05 -0400

    Updating version check for move to Github

--- elements/version.php
+++ elements/version.php
@@ -60,7 +60,7 @@ class JFormFieldVersion extends JFormField
 
 			// The module's version
 			$version = $data['version'];
-			$name    = str_replace(array('je-','-','construct','bootstruct','pro','community','core'),array(' ',' ','Construct','Bootstruct','Pro','Community','Core'), $data['name']);
+			$name    = str_replace(array('construct','bootstruct'),array('Construct','Bootstruct'), $data['name']);
 
 			// The target to check against
 			$target = 'http://construct-framework.com/upgradecheck/' . $data['name'];
