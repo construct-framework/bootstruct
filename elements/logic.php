78858ddf137f61f5067510389f559c1179548a16
Matt Thomas <matt@betweenbrain.com>
2012-12-11 21:27:03 -0500

    Adding Google Web Font subset support. Thanks CSnipper!

--- elements/logic.php
+++ elements/logic.php
@@ -9,7 +9,7 @@
 
 // Call the Construct Template Helper Class
 if (JFile::exists(dirname(__FILE__) . '/helper.php')) {
-	include dirname(__FILE__) . '/helper.php';
+    include dirname(__FILE__) . '/helper.php';
 }
 
 // To get an application object
@@ -37,10 +37,13 @@ $enableSwitcher          = $this->params->get('enableSwitcher');
 $fluidMedia              = $this->params->get('fluidMedia');
 $fullWidth               = $this->params->get('fullWidth');
 $googleWebFont           = $this->params->get('googleWebFont');
+$googleWebFontSubset     = $this->params->get('googleWebFontSubset');
 $googleWebFontTargets    = htmlspecialchars($this->params->get('googleWebFontTargets'));
 $googleWebFont2          = $this->params->get('googleWebFont2');
+$googleWebFontSubset2    = $this->params->get('googleWebFontSubset2');
 $googleWebFontTargets2   = htmlspecialchars($this->params->get('googleWebFontTargets2'));
 $googleWebFont3          = $this->params->get('googleWebFont3');
+$googleWebFontSubset3    = $this->params->get('googleWebFontSubset3');
 $googleWebFontTargets3   = htmlspecialchars($this->params->get('googleWebFontTargets3'));
 $gridSystem              = $this->params->get('gridSystem');
 $IECSS3                  = $this->params->get('IECSS3');
@@ -79,7 +82,7 @@ $version = $data['version'];
 
 // Define fallback version number for custom style sheet
 if ($customStyleSheetVersion == '') {
-	$customStyleSheetVersion = $version;
+    $customStyleSheetVersion = $version;
 }
 
 // Change generator tag
@@ -87,21 +90,21 @@ $this->setGenerator($setGeneratorTag);
 
 // Enable Mootols
 if ($loadMoo) {
-	JHtml::_('behavior.framework', true);
+    JHtml::_('behavior.framework', TRUE);
 }
 
 // Enable modal pop-ups
 if ($loadMoo && $loadModal) {
-	JHtml::_('behavior.modal');
+    JHtml::_('behavior.modal');
 }
 
 // Remove MooTools if set to no.
 if (!$loadMoo) {
-	unset($doc->_scripts[$this->baseurl . '/media/system/js/mootools-core.js']);
-	unset($doc->_scripts[$this->baseurl . '/media/system/js/mootools-more.js']);
-	unset($doc->_scripts[$this->baseurl . '/media/system/js/core.js']);
-	unset($doc->_scripts[$this->baseurl . '/media/system/js/caption.js']);
-	unset($doc->_scripts[$this->baseurl . '/media/system/js/modal.js']);
+    unset($doc->_scripts[$this->baseurl . '/media/system/js/mootools-core.js']);
+    unset($doc->_scripts[$this->baseurl . '/media/system/js/mootools-more.js']);
+    unset($doc->_scripts[$this->baseurl . '/media/system/js/core.js']);
+    unset($doc->_scripts[$this->baseurl . '/media/system/js/caption.js']);
+    unset($doc->_scripts[$this->baseurl . '/media/system/js/modal.js']);
 }
 
 // Change Google Web Font name for CSS
@@ -208,11 +211,9 @@ if ($columnGroupBetaCount) : $columnGroupBetaClass = 'span' . 12 / $columnGroupB
 $columnLayout = 'main-only';
 
 if (($columnGroupAlphaCount > 0) && ($columnGroupBetaCount == 0)) :
-	$columnLayout = 'alpha-' . $columnGroupAlphaCount . '-main';
-elseif (($columnGroupAlphaCount > 0) && ($columnGroupBetaCount > 0)) :
-	$columnLayout = 'alpha-' . $columnGroupAlphaCount . '-main-beta-' . $columnGroupBetaCount;
-elseif (($columnGroupAlphaCount == 0) && ($columnGroupBetaCount > 0)) :
-	$columnLayout = 'main-beta-' . $columnGroupBetaCount;
+    $columnLayout = 'alpha-' . $columnGroupAlphaCount . '-main'; elseif (($columnGroupAlphaCount > 0) && ($columnGroupBetaCount > 0)) :
+    $columnLayout = 'alpha-' . $columnGroupAlphaCount . '-main-beta-' . $columnGroupBetaCount; elseif (($columnGroupAlphaCount == 0) && ($columnGroupBetaCount > 0)) :
+    $columnLayout = 'main-beta-' . $columnGroupBetaCount;
 endif;
 
 #-------------------------------- Item ID ---------------------------------#
@@ -222,50 +223,48 @@ $itemId = JRequest::getInt('Itemid', 0);
 #------------------------------- Article ID -------------------------------#
 
 if ($view == 'article')
-	$articleId = JRequest::getInt('id');
+    $articleId = JRequest::getInt('id');
 else ($articleId = NULL);
 
 #------------------------------- Section ID -------------------------------#
 
-function getSection($id)
-{
-	$database = JFactory::getDBO();
-	if ((substr(JVERSION, 0, 3) >= '1.6')) {
-		return NULL;
-	} elseif (JRequest::getCmd('view', 0) == "section") {
-		return $id;
-	}
-	elseif (JRequest::getCmd('view', 0) == "category") {
-		$sql = "SELECT section FROM #__categories WHERE id = $id ";
-		$database->setQuery($sql);
-		return $database->loadResult();
-	}
-	elseif (JRequest::getCmd('view', 0) == "article") {
-		$temp = explode(":", $id);
-		$sql  = "SELECT sectionid FROM #__content WHERE id = " . $temp[0];
-		$database->setQuery($sql);
-		return $database->loadResult();
-	}
+function getSection($id) {
+    $database = JFactory::getDBO();
+    if ((substr(JVERSION, 0, 3) >= '1.6')) {
+        return NULL;
+    } elseif (JRequest::getCmd('view', 0) == "section") {
+        return $id;
+    } elseif (JRequest::getCmd('view', 0) == "category") {
+        $sql = "SELECT section FROM #__categories WHERE id = $id ";
+        $database->setQuery($sql);
+
+        return $database->loadResult();
+    } elseif (JRequest::getCmd('view', 0) == "article") {
+        $temp = explode(":", $id);
+        $sql  = "SELECT sectionid FROM #__content WHERE id = " . $temp[0];
+        $database->setQuery($sql);
+
+        return $database->loadResult();
+    }
 }
 
 $sectionId = getSection(JRequest::getInt('id'));
 
 #------------------------------ Category ID -------------------------------#
 
-function getCategory($id)
-{
-	$database = JFactory::getDBO();
-	if (JRequest::getCmd('view', 0) == "section") {
-		return null;
-	} elseif ((JRequest::getCmd('view', 0) == "category") || (JRequest::getCmd('view', 0) == "categories")) {
-		return $id;
-	}
-	elseif (JRequest::getCmd('view', 0) == "article") {
-		$temp = explode(":", $id);
-		$sql  = "SELECT catid FROM #__content WHERE id = " . $temp[0];
-		$database->setQuery($sql);
-		return $database->loadResult();
-	}
+function getCategory($id) {
+    $database = JFactory::getDBO();
+    if (JRequest::getCmd('view', 0) == "section") {
+        return NULL;
+    } elseif ((JRequest::getCmd('view', 0) == "category") || (JRequest::getCmd('view', 0) == "categories")) {
+        return $id;
+    } elseif (JRequest::getCmd('view', 0) == "article") {
+        $temp = explode(":", $id);
+        $sql  = "SELECT catid FROM #__content WHERE id = " . $temp[0];
+        $database->setQuery($sql);
+
+        return $database->loadResult();
+    }
 }
 
 $catId = getCategory(JRequest::getInt('id'));
@@ -274,22 +273,21 @@ $catId = getCategory(JRequest::getInt('id'));
 
 if ($catId && ($inheritStyle || $inheritLayout)) {
 
-	function getParentCategory($id)
-	{
-		$database = JFactory::getDBO();
-		$sql      = "SELECT parent_id
+    function getParentCategory($id) {
+        $database = JFactory::getDBO();
+        $sql      = "SELECT parent_id
         FROM #__categories
         WHERE id = $id";
-		$database->setQuery($sql);
-		return $database->loadResult();
-	}
+        $database->setQuery($sql);
 
-	$parentCategory = getParentCategory($catId);
+        return $database->loadResult();
+    }
 
-	function getAncestorCategories($id)
-	{
-		$database = JFactory::getDBO();
-		$sql      = "SELECT b.id, b.title
+    $parentCategory = getParentCategory($catId);
+
+    function getAncestorCategories($id) {
+        $database = JFactory::getDBO();
+        $sql      = "SELECT b.id, b.title
         FROM #__categories a,
         #__categories b
         WHERE a.id = $id
@@ -297,15 +295,16 @@ if ($catId && ($inheritStyle || $inheritLayout)) {
         AND a.rgt < b.rgt
         AND a.id <> b.id
         AND b.lft > 0";
-		$database->setQuery($sql);
-		return $database->loadObjectList();
-	}
+        $database->setQuery($sql);
+
+        return $database->loadObjectList();
+    }
 }
 
 #--------------------------------- Alias ----------------------------------#
 
 if ($itemId) {
-	$currentAlias = $app->getMenu()->getActive()->alias;
+    $currentAlias = $app->getMenu()->getActive()->alias;
 }
 
 #----------------------------- Component Name -----------------------------#
@@ -326,20 +325,20 @@ $styleOverride->includeFile[] = $template . '/css/item/item-' . $itemId . '.css'
 $styleOverride->includeFile[] = $template . '/css/category/' . $overrideTheme . '-category-' . $catId . '.css';
 $styleOverride->includeFile[] = $template . '/css/category/category-' . $catId . '.css';
 if ($catId && $inheritStyle) {
-	$styleOverride->includeFile[] = $template . '/css/category/category-' . $parentCategory . '.css';
+    $styleOverride->includeFile[] = $template . '/css/category/category-' . $parentCategory . '.css';
 
-	$results = getAncestorCategories($catId);
-	if (count($results) > 0) {
-		foreach ($results as $result) {
-			$styleOverride->includeFile[] = $template . '/css/category/category-' . $result->id . '.css';
-		}
-	}
+    $results = getAncestorCategories($catId);
+    if (count($results) > 0) {
+        foreach ($results as $result) {
+            $styleOverride->includeFile[] = $template . '/css/category/category-' . $result->id . '.css';
+        }
+    }
 }
 if ($view == 'category') {
-	$styleOverride->includeFile[] = $template . '/css/category/category.css';
+    $styleOverride->includeFile[] = $template . '/css/category/category.css';
 }
 if ($view == 'categories') {
-	$styleOverride->includeFile[] = $template . '/css/category/categories.css';
+    $styleOverride->includeFile[] = $template . '/css/category/categories.css';
 }
 $styleOverride->includeFile[] = $template . '/css/section/' . $overrideTheme . '-section-' . $sectionId . '.css';
 $styleOverride->includeFile[] = $template . '/css/section/section-' . $sectionId . '.css';
@@ -373,20 +372,20 @@ $layoutOverride->includeFile[] = $template . '/layouts/item/item-' . $itemId . '
 $layoutOverride->includeFile[] = $template . '/layouts/category/' . $overrideTheme . '-category-' . $catId . '.php';
 $layoutOverride->includeFile[] = $template . '/layouts/category/category-' . $catId . '.php';
 if ($catId && $inheritLayout) {
-	$layoutOverride->includeFile[] = $template . '/layouts/category/category-' . $parentCategory . '.php';
+    $layoutOverride->includeFile[] = $template . '/layouts/category/category-' . $parentCategory . '.php';
 
-	$results = getAncestorCategories($catId);
-	if (count($results) > 0) {
-		foreach ($results as $result) {
-			$layoutOverride->includeFile[] = $template . '/layouts/category/category-' . $result->id . '.php';
-		}
-	}
+    $results = getAncestorCategories($catId);
+    if (count($results) > 0) {
+        foreach ($results as $result) {
+            $layoutOverride->includeFile[] = $template . '/layouts/category/category-' . $result->id . '.php';
+        }
+    }
 }
 if ($view == 'category') {
-	$layoutOverride->includeFile[] = $template . '/layouts/category/category.php';
+    $layoutOverride->includeFile[] = $template . '/layouts/category/category.php';
 }
 if ($view == 'categories') {
-	$layoutOverride->includeFile[] = $template . '/layouts/category/categories.php';
+    $layoutOverride->includeFile[] = $template . '/layouts/category/categories.php';
 }
 $layoutOverride->includeFile[] = $template . '/layouts/section/' . $overrideTheme . '-section-' . $sectionId . '.php';
 $layoutOverride->includeFile[] = $template . '/layouts/section/section-' . $sectionId . '.php';
@@ -430,39 +429,51 @@ $doc->addStyleSheet($template . '/css/bootstrap.min.css?' . $version, 'text/css'
 $doc->addStyleSheet($template . '/css/screen.css?' . $version, 'text/css', 'screen');
 $doc->addStyleSheet($template . '/css/print.css?' . $version, 'text/css', 'print');
 if ($gridSystem != '-1') {
-	$doc->addStyleSheet($template . '/css/grids/' . $gridSystem . '?' . $version, 'text/css', 'screen');
+    $doc->addStyleSheet($template . '/css/grids/' . $gridSystem . '?' . $version, 'text/css', 'screen');
 }
 if ($customStyleSheet > -1) {
-	$doc->addStyleSheet($template . '/css/' . $customStyleSheet . '?' . $customStyleSheetVersion, 'text/css', 'screen');
+    $doc->addStyleSheet($template . '/css/' . $customStyleSheet . '?' . $customStyleSheetVersion, 'text/css', 'screen');
 }
 if ($this->direction == 'rtl') {
-	$doc->addStyleSheet($template . '/css/rtl.css?' . $version, 'text/css', 'screen');
+    $doc->addStyleSheet($template . '/css/rtl.css?' . $version, 'text/css', 'screen');
 }
 // Override style sheet returned from our template helper
 $cssFile = $styleOverride->getIncludeFile();
 if ($cssFile) {
-	$doc->addStyleSheet($cssFile . '?' . $version, 'text/css', 'screen');
+    $doc->addStyleSheet($cssFile . '?' . $version, 'text/css', 'screen');
 }
 
 // Style sheet switcher
 if ($enableSwitcher) {
-	$doc->addCustomTag('<link rel="alternate stylesheet" href="' . $template . '/css/diagnostic.css" type="text/css" media="screen" title="diagnostic" />');
-	$doc->addCustomTag('<link rel="alternate stylesheet" href="' . $template . '/css/wireframe.css" type="text/css" media="screen" title="wireframe" />');
-	$doc->addScript($template . '/js/styleswitch.js');
+    $doc->addCustomTag('<link rel="alternate stylesheet" href="' . $template . '/css/diagnostic.css" type="text/css" media="screen" title="diagnostic" />');
+    $doc->addCustomTag('<link rel="alternate stylesheet" href="' . $template . '/css/wireframe.css" type="text/css" media="screen" title="wireframe" />');
+    $doc->addScript($template . '/js/styleswitch.js');
 }
 
 // Typography
 if ($googleWebFont) {
-	$doc->addStyleSheet('http://fonts.googleapis.com/css?family=' . $googleWebFont . '');
-	$doc->addStyleDeclaration($googleWebFontTargets . ' {font-family:' . $googleWebFontFamily . ', serif;}');
+    $googleWebFont = 'http://fonts.googleapis.com/css?family=' . $googleWebFont;
+    if ($googleWebFontSubset) {
+        $googleWebFont = $googleWebFont . '&subset=' . $googleWebFontSubset;
+    }
+    $doc->addStyleSheet($googleWebFont);
+    $doc->addStyleDeclaration($googleWebFontTargets . ' {font-family:' . $googleWebFontFamily . ', serif;}');
 }
 if ($googleWebFont2) {
-	$doc->addStyleSheet('http://fonts.googleapis.com/css?family=' . $googleWebFont2 . '');
-	$doc->addStyleDeclaration($googleWebFontTargets2 . ' {font-family:' . $googleWebFontFamily2 . ', serif;}');
+    $googleWebFont2 = 'http://fonts.googleapis.com/css?family=' . $googleWebFont2;
+    if ($googleWebFontSubset) {
+        $googleWebFont2 = $googleWebFont2 . '&subset=' . $googleWebFontSubset2;
+    }
+    $doc->addStyleSheet($googleWebFont2);
+    $doc->addStyleDeclaration($googleWebFontTargets2 . ' {font-family:' . $googleWebFontFamily2 . ', serif;}');
 }
 if ($googleWebFont3) {
-	$doc->addStyleSheet('http://fonts.googleapis.com/css?family=' . $googleWebFont3 . '');
-	$doc->addStyleDeclaration($googleWebFontTargets3 . ' {font-family:' . $googleWebFontFamily3 . ', serif;}');
+    $googleWebFont3 = 'http://fonts.googleapis.com/css?family=' . $googleWebFont3;
+    if ($googleWebFontSubset) {
+        $googleWebFont3 = $googleWebFont3 . '&subset=' . $googleWebFontSubset3;
+    }
+    $doc->addStyleSheet($googleWebFont3);
+    $doc->addStyleDeclaration($googleWebFontTargets3 . ' {font-family:' . $googleWebFontFamily3 . ', serif;}');
 }
 
 // JavaScript
@@ -473,29 +484,29 @@ $doc->addScriptDeclaration('docElement = document.documentElement;docElement.cla
 
 $doc->addScriptDeclaration('window.addEvent(\'domready\',function(){new SmoothScroll({duration:1200},window)});');
 if ($loadjQuery) {
-	$doc->addCustomTag('<script type="text/javascript" src="' . $loadjQuery . '"></script>');
-	$doc->addCustomTag('<script type="text/javascript">jQuery.noConflict();</script>');
+    $doc->addCustomTag('<script type="text/javascript" src="' . $loadjQuery . '"></script>');
+    $doc->addCustomTag('<script type="text/javascript">jQuery.noConflict();</script>');
 }
 
 // Layout Declarations
 if ($siteWidth) {
-	$doc->addStyleDeclaration('#body-container, #header-above {' . $siteWidthType . ':' . $siteWidth . $siteWidthUnit . ';}');
+    $doc->addStyleDeclaration('#body-container, #header-above {' . $siteWidthType . ':' . $siteWidth . $siteWidthUnit . ';}');
 }
 if (($siteWidthType == 'max-width') && $fluidMedia) {
-	$doc->addStyleDeclaration('img, object {max-width:100%;}');
+    $doc->addStyleDeclaration('img, object {max-width:100%;}');
 }
 if ($siteWidth && !$fullWidth) {
-	$doc->addStyleDeclaration('#header, #footer {' . $siteWidthType . ':' . $siteWidth . $siteWidthUnit . '; margin:0 auto;}');
+    $doc->addStyleDeclaration('#header, #footer {' . $siteWidthType . ':' . $siteWidth . $siteWidthUnit . '; margin:0 auto;}');
 }
 if ($useStickyFooter && $stickyFooterHeight != '') {
-	$doc->addStyleDeclaration('.sticky-footer #body-container {padding-bottom:' . $stickyFooterHeight . 'px;}');
-	$doc->addStyleDeclaration('.sticky-footer #footer {margin-top:-' . $stickyFooterHeight . 'px;height:' . $stickyFooterHeight . 'px;}');
+    $doc->addStyleDeclaration('.sticky-footer #body-container {padding-bottom:' . $stickyFooterHeight . 'px;}');
+    $doc->addStyleDeclaration('.sticky-footer #footer {margin-top:-' . $stickyFooterHeight . 'px;height:' . $stickyFooterHeight . 'px;}');
 }
 
 // Internet Explorer Fixes
 $doc->addCustomTag('<!--[if lt IE 9]>');
 $doc->addCustomTag('<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>');
 if ($IECSS3) {
-	$doc->addCustomTag('<style type="text/css">' . $IECSS3Targets . ' {behavior:url("' . $this->baseurl . '/templates/' . $this->template . '/js/PIE.htc")}</style>');
+    $doc->addCustomTag('<style type="text/css">' . $IECSS3Targets . ' {behavior:url("' . $this->baseurl . '/templates/' . $this->template . '/js/PIE.htc")}</style>');
 }
 $doc->addCustomTag('<![endif]-->');
