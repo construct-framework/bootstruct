50139d03e8501063374adb45e25a247a7bee5726
Matt Thomas <matt@betweenbrain.com>
2013-07-02 22:45:01 -0400

    Dropdown alternate layouts

---
+++ html/mod_menu/Dropdown_heading.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+?>
+<?php if ($item->parent) { ?>
+<a class="nav-header dropdown-toggle" data-toggle="dropdown" href="#"><?php echo $item->title; ?><span class="caret"></span></a><?php
+	} else {
+?><span class="nav-header"><?php echo $item->title; ?></span><?php
+}
